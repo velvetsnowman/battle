4ac782755878a04862ce62d4a6ae072a5b7bdfa8
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-25 14:34:08 +0100

    refactored previous commit

--- app.rb
+++ app.rb
@@ -3,15 +3,28 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   enable :sessions
+  #used for storing information between requests
+  #a short term information store, lives on a server
+  #allows a server to store basic piece of info
+  #like a name across multiple requests
+  #most often used to store details on a logged in user
+  #a session is a HASH
+
 
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    #here we store the names into the session rather
+    #than to an ins.var to be used in a GET
     redirect '/play'
+    #we do the above to avoid rendering a view
+    #from within a POST.
+    #we are redirected to a GET to render our view
+
   end
 
   get '/play' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-
+require './spec/features/web_helpers.rb'
 Capybara.app = Battle
 
 RSpec.configure do |config|
