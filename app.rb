d4a018d555dc076702c3fdf96579459c1fa3a98b
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-27 11:20:13 +0100

    added a game class and relevant spec file for SoC

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player.rb'
+require './lib/game.rb'
 
 class Battle < Sinatra::Base
 
@@ -29,9 +30,9 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @player_1= $player_1
+    @player_1 = $player_1
     @player_2 = $player_2
-    @player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 
