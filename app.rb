ca747dc861fd717600f0ec1f3f2e34432ef87fb1
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-30 17:47:11 +0100

    refactored

--- app.rb
+++ app.rb
@@ -29,11 +29,28 @@ class Battle < Sinatra::Base
     erb :play
   end
 
+  post '/attack' do
+    $game.attack($game.opponent_of($game.current_turn))
+    if $game.game_over?
+      redirect '/game-over'
+    else
+      redirect '/attack'
+    end
+  end
+
   get '/attack' do
     @game = $game
-    @game.attack(@game.player_2)
-    @game.switch_turns
     erb :attack
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
+
+  get '/game-over' do
+    @game = $game
+    erb :game_over
   end
 
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,12 +1,19 @@
 feature 'Attacking' do
+
   scenario 'attacks player 2' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
-end
 
-feature 'Attacking' do
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'Back'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
@@ -14,4 +21,15 @@ feature 'Attacking' do
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
+
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'Back'
+    click_button 'Attack'
+    click_button 'Back'
+    expect(page).not_to have_content 'Dave: 60HP'
+    expect(page).to have_content 'Dave: 50HP'
+  end
+
 end
